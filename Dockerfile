4f490cfe7ea02f6bd5249e13768cb168de7a4360
piulin <pepo_gonba@hotmail.com>
2023-09-01 18:00:27 +0200

    add text pre and post processing in service

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,12 @@
 FROM python:3.9-slim-buster
 
-COPY notebooks/distilbert-piu-search/checkpoint-6094 /opt/model
 COPY src /opt/src
 COPY requirements_inference.txt /opt/requirements_inference.txt
-
 WORKDIR /opt
+RUN pip install --no-cache-dir -r requirements_inference.txt
 
-RUN pip install -r requirements_inference.txt
+COPY notebooks/distilbert-piu-search/checkpoint-1125 /opt/model
+
 WORKDIR /opt/src/service
 
 ENTRYPOINT [ "python", "main.py", "/opt/model" ]
--- src/service/main.py
+++ src/service/main.py
@@ -4,6 +4,7 @@ from flask import Flask, request, jsonify
 from transformers import DistilBertForTokenClassification, AutoTokenizer
 from transformers import pipeline
 import sys
+import re
 # get first argument as checkpoint path
 checkPoint = sys.argv[1]
 
@@ -15,13 +16,40 @@ token_classifier = pipeline(
     "token-classification", model=model_cpu, tokenizer=tokenizer, aggregation_strategy="simple"
 )
 
+pattern = re.compile(r'(\b[rdsc]|hd)(\d+)([+-]?)(?=\s|$)')
+
+
+def get_char_position(input):
+    matches = pattern.finditer(input)
+    positions = []
+    for match in matches:
+        positions.append(match.span()[0] +
+                         (2 if match.group(1) == 'hd' else 1))
+    return positions
+
+
+def replace_char_position(input):
+    return pattern.sub(r'\1 \2\3', input)
+
+
+def remap_positions(pred, positions):
+    for item in pred:
+        for position in positions:
+            if item['start'] > position:
+                item['start'] -= 1
+                item['end'] -= 1
+
+
 app = Flask(__name__)
 
 
 @app.route("/predict", methods=["POST"])
 def predict():
     data = request.get_json()
-    pred = token_classifier(data['text'])
+    positions = get_char_position(data['text'])
+    input = replace_char_position(data['text'])
+    pred = token_classifier(input)
+    remap_positions(pred, positions)
     # get query from json requestjsonify(token_classifier(data['text']))
     ret = [{"class": p['entity_group'], "word": p['word'],
             "start": str(p['start']), "end": str(p['end'])} for p in pred]
